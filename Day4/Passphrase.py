3593e4188fd234532d954451152c980bbf332d06
jetzken <jetzken@users.noreply.github.com>
2019-08-15 23:14:08 +0100

    Update Passphrase.py

--- Day4/Passphrase.py
+++ Day4/Passphrase.py
@@ -1,5 +1,3 @@
-import csv
-import string
 
 lines = []
 
@@ -21,4 +19,23 @@ def Part1(line):
         if valid: total += 1
     return total
 
-print (Part1(lines))
+# print (Part1(lines))
+
+# valid if not anagram
+def Part2(line):
+    total = 0
+    for l in line:
+        passphrase = l.split()
+        valid = True
+        i = 0
+        while i < len(passphrase) and valid: # compare phrases until end of line or found
+            for j in range(len(passphrase)):
+                if j+1 < len(passphrase) and j+1 != i: # skip duplicates
+                    if ''.join(sorted(passphrase[i])) == ''.join(sorted(passphrase[j+1])): valid = False #sort letters to check for match
+            i += 1
+        if valid: total += 1
+        print (total)
+    return total
+
+print (Part2(["abcde edbca", "abc abcde geg", "oiii ioii iioi"]))
+#print (Part2(lines))
